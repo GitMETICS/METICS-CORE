e1775171b993215e508b28d1a7fea4185f1957bf
yasty-sanchez <yasty2012@gmail.com>
2024-05-30 11:30:16 -0600

    Se agrega script set de datos iniciales.

--- db_METICS.sql
+++ db_METICS.sql
@@ -1,5 +1,5 @@
 /*
-	Script para crear las tablas de la base de datos
+	SCRIPT PARA CREAR LA BASE DE DATOS
 */
 
 --Creacion de la tabla rol
@@ -305,3 +305,124 @@ BEGIN
        SET @exito=0
 
 END
+
+
+
+/*
+	SCRIPT SET DE DATOS INICIAL
+*/
+
+--Crear roles
+INSERT INTO rol
+(rol_PK, nombre_rol)
+VALUES
+(0, 'Usuario'),
+(1, 'Administrador'),
+(2, 'Asesor')
+
+--Crear usuarios
+	-- admin
+EXEC InsertarUsuario @id='0000', @contrasena='#Q+3n?OWk3i0:qG'
+UPDATE usuario SET rol_FK = 1 WHERE id_usuario_PK = '0000'
+
+EXEC InsertarUsuario @id='1111', @contrasena='1234'
+UPDATE usuario SET rol_FK = 1 WHERE id_usuario_PK = '1111'
+
+EXEC InsertarUsuario @id='2222', @contrasena='1234'
+UPDATE usuario SET rol_FK = 2 WHERE id_usuario_PK = '2222'
+
+EXEC InsertarUsuario @id='3333', @contrasena='1234'
+UPDATE usuario SET rol_FK = 2 WHERE id_usuario_PK = '3333'
+
+EXEC InsertarUsuario @id='4444', @contrasena='1234'
+
+EXEC InsertarUsuario @id='5555', @contrasena='1234'
+
+--Crear asesores
+INSERT INTO asesor
+(id_usuario_FK, id_asesor_PK, nombre, apellido_1, apellido_2, telefonos, descripcion)
+VALUES
+('2222', '2222', 'Julio', 'Castro','Madriz','800800800','Soy asesor en el área de sistemas')
+
+INSERT INTO asesor
+(id_usuario_FK, id_asesor_PK, nombre, apellido_1, apellido_2, telefonos, descripcion)
+VALUES
+('3333', '3333', 'Andrés', 'Quiros','Ruiz','900900900','Soy asesor con mucha experiencia')
+
+--Crear participantes
+INSERT INTO participante(
+    id_usuario_FK, id_participante_PK, tipo_identificacion, correo, nombre, apellido_1, apellido_2 ,condicion, unidad_academica, tipo_participante,telefonos,area,departamento,seccion
+)
+VALUES
+('4444', '4444', 'Cédula','jhondoo@ucr.ac.cr', 'Jhon', 'Doo',' ', 'Interino','CICA', 'Docente','800800800','Área de Artes y Letras','Facultad de Artes','Escuela de Artes Dramáticas')
+
+INSERT INTO participante(
+    id_usuario_FK, id_participante_PK, tipo_identificacion, correo, nombre, apellido_1, apellido_2 ,condicion, unidad_academica, tipo_participante,telefonos,area,departamento,seccion
+)
+VALUES
+('5555', '5555', 'Cédula','armandotorres_rojas@ucr.ac.cr', 'Armando', 'Torres','Rojas', 'Interino','Escuela de Geología', 'Docente','900900900','Área de Artes y Letras','Facultad de Letras','Escuela de Filología, Lingüística y Literatura')
+
+--Crear tipos de actividades
+INSERT INTO tipos_actividad
+(nombre, descripcion)
+VALUES
+('Curso','Los cursos cuentan con una duración de cinco a seis horas semanales, en la que se realizarán tareas, exámenes y quices para evaluar los conceptos aprendidos por los estudiantes'),
+('Taller','Un taller es una metodología de trabajo que se caracteriza por la investigación, el aprendizaje por descubrimiento y el trabajo en equipo'),
+('Taller corto','Un taller de menos duración de dos a tres horas semanales donde se trabajara con un tema en específico, no hay examenes el objetivo es que aprendan el concepto tratado'),
+('Charla','Se realizarán charlas con profesionales invitados')
+
+--Crear categorias
+INSERT INTO categoria
+(nombre, descripcion)
+VALUES
+('Primeros pasos en la plataforma','Apenderá de manera básica las funciones del sitio de Mediación Virtual'),
+('Evaluaciones y calificaciones','Aprenderá a realizar el proceso de evaluacion y calificacion de los trabajos dentro de la plataforma'),
+('Material audio y visual','Utilice las herramientas audiovisuales que cuenta el sistema, suba y comparta material audiovisual a los estudiantes')
+
+--Crear temas
+INSERT INTO tema
+(nombre, id_categoria_FK, id_tipos_actividad_FK)
+VALUES('¿Cómo utilizar la plataforma?', 1, 1),
+('Evaluaciones en la plataforma', 2, 2),
+('Subir material audiovisual a la plataforma', 3, 3),
+('Tema Adicional', 1, 2)
+
+--Declarar archivo que se va a adjuntar (RUTA DEL ARCHIVO, Ejm: C:\Users\UserName\Documents\Folder\FileName.pdf) 
+DECLARE @adjunto varbinary(max)
+SELECT @adjunto = CAST(bulkcolumn AS varbinary(max))
+FROM OPENROWSET(BULK 'FileName.pdf', SINGLE_BLOB) AS x
+
+--Crear grupos
+INSERT INTO dbo.grupo(
+	id_tema_FK, modalidad, cupo,cantidad_horas,
+	descripcion, es_visible, lugar,
+	nombre, horario,
+	fecha_inicio_grupo, fecha_finalizacion_grupo,
+	fecha_inicio_inscripcion, fecha_finalizacion_inscripcion,
+	nombre_archivo, adjunto)
+	VALUES
+	(3,'Presencial', 15, 3,
+	'Taller de videos interactivos', 1,'Universidad de Costa Rica, Rodrigo Facio',
+	'Capacitación-Plataforma','V-S de 4pm a 7pm',
+	'2023-06-01 00:00:00','2026-12-02 00:00:00',
+	'2023-01-01 00:00:00','2026-01-01 00:00:00','ArchivoPrueba.pdf', @adjunto),
+	(2,'Virtual',10,3,
+	'Aprenda a realizar evaluaciones en la plataforma',1,'mediación virtual',
+	'Capacitación-Profesores','L-V de 4pm a 7pm',
+	'2023-06-01 00:00:00','2026-12-02 00:00:00',
+	'2023-01-01 00:00:00','2026-01-01 00:00:00','ArchivoPrueba.pdf', @adjunto),
+	(3,'Virtual',10,3,
+	'Aprenda a realizar videos llamativos e interesantes del temario del curso',1,'mediación virtual',
+	'Capacitaciones-Audiovisuales','L-M de 4pm a 7pm',
+	'2023-06-01 00:00:00','2026-12-02 00:00:00',
+	'2023-01-01 00:00:00','2026-01-01 00:00:00','ArchivoPrueba.pdf', @adjunto);
+
+
+--Crear asesor da tema
+INSERT INTO asesor_da_tema
+(id_tema_FK,id_asesor_FK,asesores_asistentes)
+VALUES
+(1, '2222', 'Julio Castro Madriz/Juan Quiros Ruiz/'),
+(2, '3333', 'Juan Quiros Ruiz/Julio Castro Madriz/'),
+(3, '2222', 'Julio Castro Madriz/Juan Quiros Ruiz/'),
+(4, '3333', 'Juan Quiros Ruiz/Julio Castro Madriz/')
