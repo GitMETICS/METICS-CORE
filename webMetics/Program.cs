b15c1c1ab3b47b9b69504180296c4e356306e627
yasty-sanchez <yasty2012@gmail.com>
2024-06-02 12:47:40 -0600

    Deleted Identity.

--- webMetics/Program.cs
+++ webMetics/Program.cs
@@ -6,17 +6,6 @@ namespace webMetics
         {
             var builder = WebApplication.CreateBuilder(args);
 
-                    // Add services to the container.
-            /*var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-                builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(connectionString));
-
-            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-            builder.Services.AddDefaultIdentity<webMeticsUser>(options => options.SignIn.RequireConfirmedAccount = false)
-                .AddRoles<IdentityRole>()
-                .AddEntityFrameworkStores<ApplicationDbContext>();*/
-            
             builder.Services.AddControllersWithViews();
             builder.Services.AddRazorPages();
             builder.Services.AddDataProtection();
@@ -47,42 +36,7 @@ namespace webMetics
                 pattern: "{controller=Home}/{action=Index}/{id?}");
             app.MapRazorPages();
 
-            /* using (var scope = app.Services.CreateScope())
-            {
-                var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-
-                var roles = new[] { "Admin", "Asesor", "Participante" };
-
-                foreach (var role in roles)
-                {
-                    if (!await roleManager.RoleExistsAsync(role))
-                        await roleManager.CreateAsync(new IdentityRole(role));
-                }
-            }
-
-            using (var scope = app.Services.CreateScope())
-            {
-                var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
-
-                string email = "admin@admin.com";
-                string password = "Test1234.";
-
-                if(await userManager.FindByEmailAsync(email) == null)
-                {
-                    var user = new IdentityUser();
-                    user.UserName = "Admin";
-                    user.Email = email;
-                    user.EmailConfirmed = true;
-
-                    await userManager.CreateAsync(user, password);
-
-                    await userManager.AddToRoleAsync(user, "Admin");
-
-                }
-            }*/
-
             app.Run();
-
         }
     }
 }
