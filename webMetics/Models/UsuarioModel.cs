41e29b4dbef16773e4eeab3921f6adc98fa539f6
yasty-sanchez <yasty2012@gmail.com>
2024-07-02 11:23:56 -0600

    Se agregan más campos en el formulario de registro.

--- webMetics/Models/UsuarioModel.cs
+++ webMetics/Models/UsuarioModel.cs
@@ -1,27 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace webMetics.Models
 {
     public class UsuarioModel
     {
-        [Required(ErrorMessage = "Es necesario ingresar un número de identificación.")]
+        // [RegularExpression(@"^\d+-\d+-\d+$", ErrorMessage = "El formato del campo de identificación debe ser 'X-XXXX-XXXX'.")]
+        [Required(ErrorMessage = "Es necesario ingresar una identificación.")]
         [Display(Name = "Número de identificación")]
-        public string identificacion { get; set; }
+        public required string identificacion { get; set; }
+
+        [Required(ErrorMessage = "Es necesario ingresar un nombre.")]
+        [Display(Name = "Nombre")]
+        public required string nombre { get; set; }
+
+        [Required(ErrorMessage = "Es necesario ingresar un apellido.")]
+        [Display(Name = "Primer apellido")]
+        public required string apellido_1 { get; set; }
+
+        [Display(Name = "Segundo apellido")]
+        public string? apellido_2 { get; set; }
 
         [RegularExpression(@"[\w\.]+@ucr\.ac\.cr", ErrorMessage = "El correo electrónico debe terminar con '@ucr.ac.cr'.")]
         [Required(ErrorMessage = "Es necesario ingresar un correo institucional.")]
         [Display(Name = "Correo institucional")]
-        public string correo { get; set; }
+        public required string correo { get; set; }
+
+        [Required(ErrorMessage = "Es necesario ingresar un tipo de identificación.")]
+        [Display(Name = "Tipo de identificación")]
+        public required string tipoIdentificacion { get; set; }
+
+        [Required(ErrorMessage = "Es necesario ingresar un tipo de participante.")]
+        [Display(Name = "Tipo de participante")]
+        public string? tipoParticipante { get; set; }
+
+        [Required(ErrorMessage = "Es necesario ingresar una unidad académica.")]
+        [Display(Name = "Unidad académica")]
+        public string? unidadAcademica { get; set; }
+
+        [Required(ErrorMessage = "Es necesario ingresar un área.")]
+        [Display(Name = "Área a la que pertenece")]
+        public string? area { get; set; }
+
+        [Required(ErrorMessage = "Es necesario ingresar un departamento.")]
+        [Display(Name = "Departamento al que pertenece")]
+        public string? departamento { get; set; }
+
+        [Required(ErrorMessage = "Es necesario ingresar una sección.")]
+        [Display(Name = "Sección a la que pertenece")]
+        public string? seccion { get; set; }
+
+        [Required(ErrorMessage = "Es necesario ingresar una condición actual.")]
+        [Display(Name = "Condición actual")]
+        public string? condicion { get; set; }
+
+        [Required(ErrorMessage = "Es necesario ingresar un número de teléfono.")]
+        [Display(Name = "Número de teléfono")]
+        public string? telefonos { get; set; }
 
         [Required(ErrorMessage = "Es necesario ingresar una contraseña.")]
         [Display(Name = "Contraseña")]
-        public string contrasena {  get; set; }
+        public required string contrasena {  get; set; }
 
         [Required(ErrorMessage = "Es necesario confirmar la contraseña.")]
         [Display(Name = "Confirmar contraseña")]
-        public string confirmarContrasena { get; set; }
+        public required string confirmarContrasena { get; set; }
 
         public ParticipanteModel? participante { get; set; }
     }
@@ -30,7 +72,7 @@ namespace webMetics.Models
     {
         [Required(ErrorMessage = "Es necesario ingresar una identificación.")]
         [Display(Name = "Identificación")]
-        public string identificacion { get; set; }
+        public required string identificacion { get; set; }
 
         [Display(Name = "Rol")]
         public int rol { get; set; }
