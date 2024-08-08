8315ff4f1c31e177fe37b5bba81753502e50c293
yasty-sanchez <yasty2012@gmail.com>
2024-08-07 20:45:19 -0600

    Se corrige error al crear temas.

--- webMetics/Handlers/TemaHandler.cs
+++ webMetics/Handlers/TemaHandler.cs
@@ -69,7 +69,7 @@ namespace webMetics.Handlers
 
         public bool CrearTema(TemaModel tema)
         {
-            string consulta = "INSERT INTO tema nombre VALUES @nombre;";
+            string consulta = "INSERT INTO tema(nombre) VALUES (@nombre);";
 
             ConexionMetics.Open();
 
