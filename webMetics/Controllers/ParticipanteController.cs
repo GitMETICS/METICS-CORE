9bf03e9a904eca89ecfd38077c28e86d87e6ddaf
yasty-sanchez <yasty2012@gmail.com>
2024-10-21 16:20:33 -0600

    Resuelvo merge conflict.

--- webMetics/Controllers/ParticipanteController.cs
+++ webMetics/Controllers/ParticipanteController.cs
@@ -837,7 +837,6 @@ namespace webMetics.Controllers
                 ViewBag.Id = GetId();
 
                 GrupoModel grupo = accesoAGrupo.ObtenerGrupo(idGrupo.Value);
-                bool grupoIncompleto = grupo == null || grupo.esVisible == false;
 
                 ParticipanteModel participante = accesoAParticipante.ObtenerParticipante(idParticipante);
                 int nuevoTotalHorasAprobadas = participante.horasAprobadas + horasAprobadas;
@@ -849,7 +848,7 @@ namespace webMetics.Controllers
                 {
                     inscripcion.horasAprobadas = nuevasHorasAprobadas;
                     accesoAInscripcion.EditarInscripcion(inscripcion);
-                    accesoAInscripcion.CambiarEstadoDeInscripcion(inscripcion, grupoIncompleto);
+                    accesoAInscripcion.CambiarEstadoDeInscripcion(inscripcion, grupo);
 
                     if (nuevoTotalHorasAprobadas <= participante.horasMatriculadas && nuevoTotalHorasAprobadas >= 0)
                     {
