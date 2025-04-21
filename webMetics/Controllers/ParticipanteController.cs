6f23b92497aabe45b77fe0b1bc19d95bff198747
ESTEBAN ISAAC BAIRES CERDAS <esteban.baires@ucr.ac.cr>
2025-04-21 16:39:59 -0600

    Eliminar caractéres del rango para generar contraseña. ParticipanteController.cs: GenerateRandomPassword()

--- webMetics/Controllers/ParticipanteController.cs
+++ webMetics/Controllers/ParticipanteController.cs
@@ -1421,7 +1421,7 @@ namespace webMetics.Controllers
         private string GenerateRandomPassword()
         {
             int length = 10;
-            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+";
+            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
             var random = new Random();
             string password = new string(Enumerable.Repeat(chars, length)
                 .Select(s => s[random.Next(s.Length)]).ToArray());
