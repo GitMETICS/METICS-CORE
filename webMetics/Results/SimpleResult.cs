6710466797457608de758a7a171f5ec7855bf3ca
ebai03 <esteban.baires@ucr.ac.cr>
2025-07-17 13:52:05 -0600

    Clase SimpleResult para manejo simple de errores

---
+++ webMetics/Results/SimpleResult.cs
@@ -0,0 +1,29 @@
+﻿namespace webMetics.Results;
+
+public class SimpleResult<T>
+{
+    public bool IsSuccess { get; set; }
+    public string? Message { get; set; }
+    public required T Data { get; set; }
+
+    public static SimpleResult<T> Success(string message = "", T data = default!)
+    {
+        return new SimpleResult<T> { IsSuccess = true, Message = message, Data = data };
+    }
+
+    public static SimpleResult<T> Error(string message, T data = default!)
+    {
+        return new SimpleResult<T> { IsSuccess = false, Message = message, Data = data };
+    }
+}
+
+public static class SimpleResultExtensions
+{
+    public static SimpleResult<T> ToSimpleResult<T>(this T value, string notFoundMessage = "Elemento no encontrado")
+        where T : class
+    {
+        return value != null
+            ? SimpleResult<T>.Success(data: value)
+            : SimpleResult<T>.Error(notFoundMessage);
+    }
+}
