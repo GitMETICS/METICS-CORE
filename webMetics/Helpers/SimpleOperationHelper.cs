2b231735eef6c63f57bc3e06b26420b752516baa
ebai03 <esteban.baires@ucr.ac.cr>
2025-07-17 13:53:16 -0600

    Wrapper y Helper con SimpleResult para ejecución de funciones

---
+++ webMetics/Helpers/SimpleOperationHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using webMetics.Results;
+
+namespace webMetics.Helpers;
+
+public static class SimpleOperationHelper
+{
+    public static SimpleResult<T> Execute<T>(Func<T> operation, string successMessage = "", string errorMessage = "")
+    {
+        try
+        {
+            var result = operation();
+            return SimpleResult<T>.Success(successMessage, result);
+        }
+        catch (Exception ex)
+        {
+            return SimpleResult<T>.Error(errorMessage ?? ex.Message);
+        }
+    }
+}
---
+++ webMetics/Helpers/SimpleOperationWrapperHelper.cs
@@ -0,0 +1,22 @@
+﻿using webMetics.Results;
+
+namespace webMetics.Helpers;
+
+public static class SimpleOperationWrapperHelper
+{
+    public static SimpleResult<T> ExecuteWithNullCheck<T>(Func<T> operation, string? notFoundMessage = null, string? errorMessage = null)
+        where T : class
+    {
+        try
+        {
+            var result = operation();
+            return result != null
+                ? SimpleResult<T>.Success(data: result)
+                : SimpleResult<T>.Error(notFoundMessage ?? "Elemento no encontrado");
+        }
+        catch (Exception ex)
+        {
+            return SimpleResult<T>.Error(errorMessage ?? ex.Message);
+        }
+    }
+}
