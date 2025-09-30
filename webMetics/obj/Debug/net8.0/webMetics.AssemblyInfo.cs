97d5979f0aac05817db40684480602badbef387a
yasty-sanchez <yasty2012@gmail.com>
2025-09-29 20:28:33 -0600

    Debug Checkpoint

--- webMetics/obj/Debug/net8.0/webMetics.AssemblyInfo.cs
+++ webMetics/obj/Debug/net8.0/webMetics.AssemblyInfo.cs
@@ -11,12 +11,11 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("aspnet-webMetics-2793fa9b-a054-47cf-8443-da5f8bca4169")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("webMetics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ead3c8bc4ba241e29cecc4c7f21822a7924197e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a7c0750d4432624e88b6e49a821cffc6d9e3bb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("webMetics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webMetics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
