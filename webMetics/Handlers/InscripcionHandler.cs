317d03076807f1dec104736dbf028491dbc29d29
ebai03 <ESTEBAN.BAIRES@ucr.ac.cr>
2025-11-17 14:07:43 -0600

    Arreglo en asignación de estado a Inscripciones

--- webMetics/Handlers/InscripcionHandler.cs
+++ webMetics/Handlers/InscripcionHandler.cs
@@ -426,17 +426,13 @@ public class InscripcionHandler : BaseDeDatosHandler
     {
         string estado;
 
-        if (inscripcion.horasAprobadas >= inscripcion.horasMatriculadas && inscripcion.horasAprobadas > 0)
-
-        //if (inscripcion.horasAprobadas >= (inscripcion.horasMatriculadas + inscripcion.horasAprobadas) && inscripcion.horasAprobadas > 0)
+        if (inscripcion.horasMatriculadas <= 0 && inscripcion.horasAprobadas > 0)
         {
             estado = "Aprobado";
         }
         else
         {
-            if (inscripcion.horasAprobadas < inscripcion.horasMatriculadas && inscripcion.horasAprobadas > 0)
-
-            //if (inscripcion.horasAprobadas < (inscripcion.horasMatriculadas + inscripcion.horasAprobadas) && inscripcion.horasAprobadas > 0)
+            if (inscripcion.horasAprobadas > 0)
             {
                 estado = "Incompleto";
             }
