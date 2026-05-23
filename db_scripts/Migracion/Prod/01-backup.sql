dac3256438c4cd6c1904eeac6ddf437833847528
ebai03 <ESTEBAN.BAIRES@ucr.ac.cr>
2026-05-23 00:55:41 -0600

    chore: Add comments on backup db script

--- db_scripts/Migracion/Prod/01-backup.sql
+++ db_scripts/Migracion/Prod/01-backup.sql
@@ -1,8 +1,9 @@
+-- Respaldar base de datos SIMETICS a un archivo .bak, se debe tener un volumen de host montado en /var/opt/mssql/backup para que el contenedor de SQL Server pueda escribir el archivo de respaldo.
 BACKUP DATABASE [SIMETICS]
 TO DISK = N'/var/opt/mssql/backup/SIMETICS_test.bak'
 WITH FORMAT, INIT;
 
-
+-- Restaurar la base de datos SIMETICS desde el archivo .bak
 RESTORE DATABASE [SIMETICS]
 FROM DISK = N'/var/opt/mssql/backup/SIMETICS_test.bak'
 WITH REPLACE;
